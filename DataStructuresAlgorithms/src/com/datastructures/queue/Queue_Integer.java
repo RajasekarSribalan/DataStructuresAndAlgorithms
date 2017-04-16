5e0d1a196dcaec9db451777f38ca6d234deb1729
Rajasekar <emailtoraj90@gmail.com>
2017-04-16 10:35:53 +0530

    Datastructure : queue using array and linkedlist

---
+++ DataStructuresAlgorithms/src/com/datastructures/queue/Queue_Integer.java
@@ -0,0 +1,69 @@
+package com.datastructures.queue;
+
+/**
+ * Class Queue_Integer
+ * 
+ * Queue : First in First out using Array
+ * 
+ * @author raj
+ *
+ */
+public class Queue_Integer {
+
+	public int[] queue;
+	public int front;
+	public int rear;
+	public int size;
+
+	public Queue_Integer(int size) {
+		queue = new int[size];
+		this.front = 0;
+		this.rear = -1;
+	}
+
+	/**
+	 * Method : enqueue
+	 * @param data
+	 */
+	public void enqueue(int data) {
+		rear++;
+		queue[rear] = data;
+		size++;
+	}
+
+	/**
+	 * Method : dequeue
+	 * @return
+	 */
+	public int dequeue() {
+		size--;
+		return queue[front++];
+
+	}
+
+	public int peelFront() {
+		return queue[front];
+	}
+
+	public boolean isEmpty() {
+		return (size == 0);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue_Integer queue = new Queue_Integer(10);
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+		queue.enqueue(4);
+		
+		while(!queue.isEmpty()){
+			System.out.println(queue.dequeue());
+		}
+
+	}
+
+}
---
+++ DataStructuresAlgorithms/src/com/datastructures/queue/Queue_UsingLinkedList.java
@@ -0,0 +1,105 @@
+package com.datastructures.queue;
+/**
+ * Class Queue_UsingLinkedList
+ * 
+ * Queue : First in First out using LinkedList
+ * 
+ * @author raj
+ *
+ */
+public class Queue_UsingLinkedList {
+
+	public Node first, rear;
+	private int size;
+
+	public Queue_UsingLinkedList() {
+		first = null;
+		rear = null;
+		size = 0;
+	}
+
+	public class Node {
+		public Node next;
+		public int data;
+	}
+
+	public boolean isEmpty() {
+		return (size == 0);
+	}
+
+	public void enqueue(int data) {
+
+		Node oldRear = rear;
+		rear = new Node();
+		rear.data = data;
+		rear.next = null;
+		if (isEmpty()) {
+			first = rear;
+		} else {
+			oldRear.next = rear;
+		}
+		size++;
+	}
+
+	public int dequeue() {
+
+		int temp = first.data;
+		first = first.next;
+		size--;
+		return temp;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public void display() {
+		Node check = first;
+		while (null != check) {
+			System.out.print(check.data + "-->");
+			check = check.next;
+		}
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue_UsingLinkedList queue = new Queue_UsingLinkedList();
+		queue.enqueue(2);
+		queue.enqueue(4);
+		queue.enqueue(6);
+		
+		queue.display();
+
+		System.out.println("Gonnaa remove");
+
+		queue.dequeue();
+		queue.dequeue();
+		
+		queue.display();
+	}
+
+	public Node getFirst() {
+		return first;
+	}
+
+	public void setFirst(Node first) {
+		this.first = first;
+	}
+
+	public Node getRear() {
+		return rear;
+	}
+
+	public void setRear(Node rear) {
+		this.rear = rear;
+	}
+
+}
