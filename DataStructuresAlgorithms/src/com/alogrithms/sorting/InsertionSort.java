ad5db7865f8b6350cf9c42367aa6171f69c27ed3
Rajasekar <rajasekar.sribalan@globallogic.com>
2017-10-27 15:50:03 +0530

    Insertion sort in descending order

--- DataStructuresAlgorithms/src/com/alogrithms/sorting/InsertionSort.java
+++ DataStructuresAlgorithms/src/com/alogrithms/sorting/InsertionSort.java
@@ -10,19 +10,34 @@ public class InsertionSort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		/*
+		 * ASCENDING ORDER
+		 */
+
 		int arr[] = { 5, 4, 3, 2, 1 };
 		printArray("Before sorting", arr);
-		arr = insertionSort(arr);
-		printArray("After sorting", arr);
+		arr = insertionSortASC(arr);
+		printArray("After sorting in ascending order using insertion sort", arr);
+
+		/*
+		 * DESCENDING ORDER
+		 */
+
+		int arr1[] = { 1, 2, 3, 4, 5 };
+		printArray("Before sorting", arr1);
+		arr = insertionSortDESC(arr1);
+		printArray("After sorting in descending order using insertion sort",
+				arr1);
 	}
 
 	/**
-	 * Method insertionSort to perform insertion sort on array
+	 * Method insertionSort to perform insertion sort ASCENDING on array
 	 * 
 	 * @param arr
 	 * @return
 	 */
-	static int[] insertionSort(int arr[]) {
+	static int[] insertionSortASC(int arr[]) {
 
 		for (int i = 1; i < arr.length; i++) {
 			int key = arr[i];
@@ -37,6 +52,30 @@ public class InsertionSort {
 		return arr;
 
 	}
+	
+	
+	/**
+	 * Method insertionSort to perform insertion sort DESCENDING on array
+	 * 
+	 * @param arr
+	 * @return
+	 */
+	static int[] insertionSortDESC(int arr[]) {
+
+		for (int i = 1; i < arr.length; i++) {
+			int key = arr[i];
+			int j = i - 1;
+
+			while (j >= 0 && arr[j] < key) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
+		return arr;
+
+	}
+
 
 	/**
 	 * Print Array
