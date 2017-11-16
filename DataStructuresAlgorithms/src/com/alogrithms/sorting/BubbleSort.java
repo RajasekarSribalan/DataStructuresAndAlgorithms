292e7f8b7e834dc45287fd26be197a6e9d1772f7
Rajasekar <rajasekar.sribalan@globallogic.com>
2017-11-16 18:41:02 +0530

    Bubble sort - Ascending order

---
+++ DataStructuresAlgorithms/src/com/alogrithms/sorting/BubbleSort.java
@@ -0,0 +1,53 @@
+package com.alogrithms.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort
+{
+
+    public static void main(String[] args)
+    {
+        int[] array =
+        { 1, 4, 6, 11, 8, 5, 2, 19, 0 };
+
+        bubbleSortAscending(array);
+        printArray("Bubble sort in ascending order", array);
+
+    }
+
+    /**
+     * @param array
+     */
+    private static void bubbleSortAscending(int[] array)
+    {
+        int n = array.length;
+
+        while (n > 0)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+
+                if (array[i] > array[i + 1])
+                {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+            n--;
+        }
+
+    }
+
+    /**
+     * Print Array
+     * 
+     * @param message
+     * @param arr
+     */
+    private static void printArray(String message, int[] arr)
+    {
+        System.out.println(message + " : " + Arrays.toString(arr));
+
+    }
+}
